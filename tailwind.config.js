c23a60f59a54d75c5917b1afdb06cd3484b47a1d
SeniorCub <farinderif@gmail.com>
2024-11-10 17:37:17 +0100

    ......

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
-  theme: {
-    extend: {
-      container: {
-        center : true,
-      },
-      colors: {
-        pryPurple : "#796fab",
-        secBlack : "#282828",
-        secWhite : "#eff6f8",
-        accYellow : "#e5aa2d",
-        accPink : "#e487bc",
-      },
-    },
-  },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-}
+     content: [
+       "./**/*.{html,js}",  // All HTML and JS files in the root and any subfolders
+       "./src/**/*.{html,js}" // All HTML and JS files specifically inside the src folder and its subfolders
+     ],
+     theme: {
+       extend: {
+         colors: {
+           pryPurple: "#796fab",
+           secBlack: "#282828",
+           secWhite: "#eff6f8",
+           accYellow: "#e5aa2d",
+           accPink: "#e487bc",
+         },
+       },
+     },
+     plugins: [],
+   }   
